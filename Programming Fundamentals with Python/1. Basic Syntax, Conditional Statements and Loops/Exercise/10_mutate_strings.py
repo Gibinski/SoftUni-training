ff0776eb1f96d419fb35aeea6a32106ed92fb28b
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-06-12 15:17:24 +0200

    Add Basic Syntax, Conditional Statements and Loops - Exercise

--- Programming Fundamentals with Python/1. Basic Syntax, Conditional Statements and Loops/Exercise/10_mutate_strings.py
+++ Programming Fundamentals with Python/1. Basic Syntax, Conditional Statements and Loops/Exercise/10_mutate_strings.py
@@ -1,22 +1,19 @@
-# You will be given two strings. Transform the first string into the second one, 
-# letter by letter, starting from the first one. After each interaction, 
-# print the resulting string only if it is unique.
+# You will be given two strings. 
+# Transform the first string into the second one, letter by letter, starting from the first one. 
+# After each interaction, print the resulting string only if it is unique.
 
 # Note: the strings will have the same length.
 
 firs_str = input()
 second_str = input()
 previous_str = firs_str
-
+result = ""
 for i in range(len(firs_str)):
-    right_part = firs_str[:i + 1]
+    left_part = second_str[:i + 1]
     right_part = firs_str[i + 1:]
 
-    if i is previous_char:
-
+    result = left_part + right_part
+    if result == previous_str:
         continue
-    firs_str[i] = second_str[i]
-    print(firs_str)
-    previous_char = firs_str[i]
-
-
+    previous_str = result
+    print(result)
