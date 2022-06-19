da6d7e48645d3f1e8ce736e27a0b34bfae3d77ed
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-06-19 23:47:42 +0200

    Add 3. Lists Basics

--- Programming Fundamentals with Python/3. Lists Basics/Exercise/07_easter_gifts .py
+++ Programming Fundamentals with Python/3. Lists Basics/Exercise/07_easter_gifts .py
@@ -6,11 +6,11 @@ while commands != "No Money":
     if command[0] == "OutOfStock": 
         while command[1] in gifts:
             gifts[gifts.index(command[1])] = None
-    elif command[0] == "Required" and int(command[2]) <= len(gifts) - 1:
+    elif command[0] == "Required" and 0 <= int(command[2]) < len(gifts):
         gifts[int(command[2])] = command[1]
     elif command[0] == "JustInCase":
-        gifts.pop()
-        gifts.append(command[1])
+        gifts[-1] = command[1]
     commands = input()
-gifts = [gift for gift in gifts if gift != None]
-print(" ".join(gifts))
+for gift in gifts:
+    if gift != None:
+        print(gift, end=" ")
--- Programming Fundamentals with Python/3. Lists Basics/Exercise/08_seize_the_fire.py
+++ Programming Fundamentals with Python/3. Lists Basics/Exercise/08_seize_the_fire.py
@@ -1,27 +1,24 @@
-from operator import index, indexOf
-from textwrap import indent
+fires_cells  = input().split("#")
+water = int(input())
+cells = []
+total_fire = 0
 
-
-l = ["A", "AA", "AB", "B", "BA", "BB", None ]
-print(len(l))
-print(l)
-print()
-
-del l[0]
-print(len(l))
-print(l)
-print()
-
-l[0] = None
-print(len(l))
-print(l)
-print()
-
-l.remove("BB")
-print(len(l))
-print(l)
-print()
-
-l.pop(l.index(l[1]))
-print(len(l))
-print(l)
+for cell in fires_cells:
+    cells.append(cell.split(" = ")) 
+print("Cells:")
+for cell in cells:
+    type_of_fire = cell[0]
+    range = int(cell[1])
+    filter_cell = (
+        water >= range and (
+        (type_of_fire == "High" and 81 <= range <= 125) or 
+        (type_of_fire == "Medium" and 51 <= range <= 80) or 
+        (type_of_fire == "Low" and 1 <= range <= 50)  
+        )
+    )
+    if filter_cell:
+        water -= range
+        total_fire += range
+        print(f" - {range}")
+print(f"Effort: {(total_fire * 0.25):.2f}")
+print(f"Total Fire: {total_fire}")
