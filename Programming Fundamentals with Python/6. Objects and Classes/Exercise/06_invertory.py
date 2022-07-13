6edeb3399d0ba3fa71df6e72b59c1b2dc16b9aff
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-07-13 21:20:07 +0200

    add

---
+++ Programming Fundamentals with Python/6. Objects and Classes/Exercise/06_invertory.py
@@ -0,0 +1,28 @@
+class Inventory:
+
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
+        self.capacity = capacity
+        self.items = []
+
+    def add_item(self, item: str):
+        if self.capacity > 0:
+            self.items.append(item)
+            self.capacity -= 1
+        else:
+            return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+
+    def __repr__(self):
+        items = ", ".join(self.items)
+        return f"Items: {items}.\nCapacity left: {self.capacity}"
+
+inventory = Inventory(2)
+inventory.add_item("potion")
+inventory.add_item("sword")
+print(inventory.add_item("bottle"))
+print(inventory.get_capacity())
+print(inventory)
