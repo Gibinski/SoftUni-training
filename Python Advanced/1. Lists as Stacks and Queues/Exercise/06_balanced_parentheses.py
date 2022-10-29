d3ffac5b2910d0213b38845a1777ec73185dc596
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-10-29 19:25:41 +0200

    Add

---
+++ Python Advanced/1. Lists as Stacks and Queues/Exercise/06_balanced_parentheses.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+que = deque(input())
+types = {")": "(", "]": "[", "}": "{"}
+stack = []
+if len(que) % 2 == 0:
+    while que:
+        bracket = que.popleft()
+        if bracket in types.values():
+            stack.append(bracket)
+        else:
+            if stack:
+                if types[bracket] == stack[-1]:
+                    stack.pop()       
+                else:
+                    print("NO")
+                    break           
+            else:
+                print("NO")
+                break           
+    else:
+        print("YES")
+else:
+    print("NO")
