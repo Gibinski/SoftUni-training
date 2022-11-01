8940294e976ae26710dedbc16955ad52cf0a60ad
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-11-01 23:28:35 +0100

    Add Function str_range_to_set

--- Python Advanced/2. Tuples and Sets/Exercise/05_longest_intersection.py
+++ Python Advanced/2. Tuples and Sets/Exercise/05_longest_intersection.py
@@ -1,18 +1,19 @@
+def str_range_to_set(set_value):
+    set_start_value, set_end_value = tuple(set_value.split(","))
+    return set(range(int(set_start_value), int(set_end_value) + 1))
+    
+
 n = int(input())
 
-intersection = set()
 longest_intersection = set()
 for i in range(n):
     set_1_value, set_2_value = tuple(input().split("-"))
 
-    set_1_start_value, set_1_end_value = tuple(set_1_value.split(","))
-    set_1 = set(range(int(set_1_start_value), int(set_1_end_value) + 1))
-
-    set_2_start_value, set_2_end_value = tuple(set_2_value.split(","))
-    set_2 = set(range(int(set_2_start_value), int(set_2_end_value) + 1))
+    set_1 = str_range_to_set(set_1_value) 
+    set_2 = str_range_to_set(set_2_value) 
 
     intersection = set_1 & set_2
     if len(intersection) > len(longest_intersection):
-        longest_intersection = intersection.copy()
+        longest_intersection = intersection
 
 print(f"Longest intersection is {sorted(longest_intersection)} with length {len(longest_intersection)}")
