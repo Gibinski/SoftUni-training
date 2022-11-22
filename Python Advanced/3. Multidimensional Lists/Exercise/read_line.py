939bdc66fba77b1dc356459483312e1ed9164fa1
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-11-22 16:46:38 +0100

    add

--- Python Advanced/3. Multidimensional Lists/Exercise/read_line.py
+++ Python Advanced/3. Multidimensional Lists/Exercise/read_line.py
@@ -3,3 +3,10 @@ def line_to_int_list():
     for el in input().split(", "):
         line.append(int(el)) 
     return line
+
+
+def matrix_of_int(rows):
+    matrix = []
+    for i in range(rows):
+        matrix.append(line_to_int_list)
+    return matrix
