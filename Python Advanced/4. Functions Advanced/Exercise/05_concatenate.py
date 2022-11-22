a7f9fda5094a5c9fc56726b1ed738fa207c8dde7
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-11-22 19:49:58 +0100

    add

---
+++ Python Advanced/4. Functions Advanced/Exercise/05_concatenate.py
@@ -0,0 +1,9 @@
+def concatenate(*args, **kwargs):
+    word = "".join(args)
+    for key, val in kwargs.items():
+        word = word.replace(key, val)
+
+    return word
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
