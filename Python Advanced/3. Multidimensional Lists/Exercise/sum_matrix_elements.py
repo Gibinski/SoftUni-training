17df0fce947936fac849be92d3b1493a0d0869ea
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-11-22 16:13:04 +0100

    add

--- Python Advanced/3. Multidimensional Lists/Exercise/sum_matrix_elements.py
+++ Python Advanced/3. Multidimensional Lists/Exercise/sum_matrix_elements.py
@@ -1,14 +1,14 @@
-def read_line():
-    numbers = []
+def line_to_int_list():
+    line = []
     for el in input().split(", "):
-        numbers.append(int(el)) 
-    return numbers
+        line.append(int(el)) 
+    return line
 
-n, m = read_line()
+n, m = line_to_int_list()
 matrix = []
-sum_ = 0
+sum_matrix = 0
 for i in range(n):
-    matrix.append(read_line())
-    sum_ += sum(matrix[i])
-print(sum_)
+    matrix.append(line_to_int_list())
+    sum_matrix += sum(matrix[i])
+print(sum_matrix)
 print(matrix)
