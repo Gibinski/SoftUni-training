63c8b8d51cca8cb783138c195a860729042a64f1
Lyubomir Gibinski <marin4o_m@abv.bg>
2022-11-23 16:55:20 +0100

    add

--- Python Advanced/5. Error Handling/Exercise/2_email_+validator.py
+++ Python Advanced/5. Error Handling/Exercise/2_email_+validator.py
@@ -16,10 +16,10 @@ email = input()
 valid_domain = [".com", ".bg", ".org", ".net"]
 
 while email:
-    if len(findall(pattern_name, email.split("@")[0])[0]) <= 4:
-        raise NameTooShortError("Name must be more than 4 characters")
-    elif "@" not in email:
+    if "@" not in email:
         raise MustContainAtSymbolError("Email must contain @")
+    elif len(findall(pattern_name, email.split("@")[0])[0]) <= 4:
+        raise NameTooShortError("Name must be more than 4 characters")
     elif findall(pattern_domain, email.split("@")[1])[0] not in valid_domain:
         raise InvalidDomainError(f"Domain must be one of the folowing: {', '.join(valid_domain)}")
 
