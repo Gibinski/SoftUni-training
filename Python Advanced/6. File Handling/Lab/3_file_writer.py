8af4dc514f4456b08190da8cc6b9ace78d9a0258
Lyubomir Gibinski <l.gibinski@gmail.com>
2022-11-24 13:19:22 +0100

    add

--- Python Advanced/6. File Handling/Lab/3_file_writer.py
+++ Python Advanced/6. File Handling/Lab/3_file_writer.py
@@ -1,2 +1,5 @@
-with open(r".\my_first_file.txt", "w") as file:
-     file.write('I just created my first file!')
+from os import path, linesep
+
+path_file = path.join("my_first_file.txt")
+with open(path_file, "w") as file:
+     file.write('I just created my first file!' + linesep)
