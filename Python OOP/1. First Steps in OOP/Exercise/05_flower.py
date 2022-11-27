e59b8b877d6f9105f274eaf531e1d63bfb6ce2b7
Lyubomir Gibinski <l.gibinski@gmail.com>
2022-11-27 12:57:46 +0100

    add

---
+++ Python OOP/1. First Steps in OOP/Exercise/05_flower.py
@@ -0,0 +1,35 @@
+class Flower:
+
+    def __init__(self, name: str, water_requirements: int, is_happy = False):
+        self.name = name
+        self.water_requirements = water_requirements
+        self.is_happy = is_happy
+
+
+    def water(self, quantity):
+
+        if self.water_requirements <= quantity:
+            self.is_happy = True
+
+
+    def status(self):
+        y = "not "
+        if self.is_happy:
+            y = ""
+
+        return f"{self.name} is {y}happy"
+
+
+
+#test Code
+
+flower = Flower("Lilly", 100)
+
+flower.water(50)
+print(flower.status())
+
+flower.water(60)
+print(flower.status())
+
+flower.water(100)
+print(flower.status())
