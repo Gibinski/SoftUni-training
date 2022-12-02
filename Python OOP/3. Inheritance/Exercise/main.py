9e2178422f1af769a06d247df086cfe8697cb8de
Lyubomir Gibinski <l.gibinski@gmail.com>
2022-12-02 19:45:31 +0100

    Add 02. Zoo

--- Python OOP/3. Inheritance/Exercise/main.py
+++ Python OOP/3. Inheritance/Exercise/main.py
@@ -1,9 +1,16 @@
-from project.child import Child
-from project.person import Person
+from project.animal import Animal
+from project.bear import Bear
+from project.gorilla import Gorilla
+from project.lizard import Lizard
+from project.mammal import Mammal
+from project.reptile import Reptile
+from project.snake import Snake
 
-print()
-person = Person("Peter", 25)
-child = Child("Peter Junior", 5)
-print(person.name)
-print(person.age)
-print(child.__class__.__bases__[0].__name__)
+
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
