56e778d9d34f078ce2641c273df8a650df5e46b4
Lyubomir Gibinski <l.gibinski@gmail.com>
2022-12-03 17:21:46 +0100

    Add class Mammal

--- Python OOP/4. Encapsulation/Lab/2_mammal.py
+++ Python OOP/4. Encapsulation/Lab/2_mammal.py
@@ -1,3 +1,26 @@
 class Mammal:
-    def __init__(self):
-        pass
+    def __init__(self, name, type, sound):
+        self.name = name
+        self.type = type
+        self.sound = sound
+        self.__kingdom = "animals"
+
+
+    def make_sound(self):
+        return f"{self.name} makes {self.sound}"
+
+
+    def get_kingdom(self):
+        return self.__kingdom
+
+
+    def info(self):
+        return f"{self.name} is of type {self.type}"
+
+
+# Test Code
+
+# mammal = Mammal("Dog", "Domestic", "Bark")
+# print(mammal.make_sound())
+# print(mammal.get_kingdom())
+# print(mammal.info())
