5d24bbcd8d3b5b523abd8bedd0eb8e66b92afa77
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-09 15:31:59 +0100

    Add

---
+++ JS Front-End/1. JS Syntax Fundamentals/Exercises/02. Vacation.js
@@ -0,0 +1,14 @@
+function solve(group, type, day) {
+    let price;
+
+    if (day === 'Friday') {
+        if (type === 'Students') {
+            
+        }
+    }
+
+    console.log(`Total price: ${price.toFixed(2)}`)
+}
+
+solve(30,"Students","Sunday")
+solve(40,"Regular","Saturday")
