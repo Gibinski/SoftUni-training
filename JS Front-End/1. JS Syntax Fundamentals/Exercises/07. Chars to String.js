01f156b525b275a54980bd49601839ede18c13fb
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-12 12:00:22 +0100

    Add

---
+++ JS Front-End/1. JS Syntax Fundamentals/Exercises/07. Chars to String.js
@@ -0,0 +1,8 @@
+function charsToString(ch1, ch2, ch3) {
+    let result = ch1 + ch2 + ch3;
+    console.log(result);
+}
+
+charsToString('a','b','c');
+charsToString('%','2','o');
+charsToString('1','5','p');
