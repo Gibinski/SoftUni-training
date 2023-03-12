5edd6dea8457c2b1de957048db2c909a630c6ec8
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-12 07:07:23 +0100

    Add

--- JS Front-End/1. JS Syntax Fundamentals/Exercises/04. Print And Sum.js
+++ JS Front-End/1. JS Syntax Fundamentals/Exercises/04. Print And Sum.js
@@ -1,7 +1,15 @@
-function printAndSum() {
+function printAndSum(start, end) {
+    let sum = 0;
+    let output = [];
 
+    for (let currentNum = start; currentNum <= end; currentNum++) {
+        output.push(currentNum);
+        sum += currentNum;
+    }
+    console.log(output.join(" "))
+    console.log(`Sum: ${sum}`)
 }
 
-printAndSum();
-printAndSum();
-printAndSum();
+printAndSum(5, 10);
+printAndSum(0, 26);
+printAndSum(50, 60);
