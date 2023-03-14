f14cc0a243892b6a5b2c4129ad9d1785d4e05e50
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-14 12:45:58 +0100

    Add

--- JS Front-End/1. JS Syntax Fundamentals/Exercises/15. List Of Names.js
+++ JS Front-End/1. JS Syntax Fundamentals/Exercises/15. List Of Names.js
@@ -1,8 +1,8 @@
 function listOfNames(arr) {
     return arr
-    .sort((a, b) => a.localeCompare(b))
-    .map((name, index) => `${index + 1}.${name} `)
-    .join("\n");
+        .sort((a, b) => a.localeCompare(b))
+        .map((name, index) => `${index + 1}.${name} `)
+        .join("\n");
 }
 
 console.log(listOfNames(["John", "Bob", "Christina", "Ema"]));
--- JS Front-End/1. JS Syntax Fundamentals/Exercises/16. Sorting Numbers.js
+++ JS Front-End/1. JS Syntax Fundamentals/Exercises/16. Sorting Numbers.js
@@ -1,8 +1,13 @@
 function sortingNumbers(arr) {
     let resultArr = [];
-
-
+    arr.sort((a, b) => a - b);
+    while (arr.length) {
+        resultArr.push(arr.shift());
+        if (arr.length) {
+            resultArr.push(arr.pop());
+        }
+    }  
     return resultArr;
 }
 
-sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
