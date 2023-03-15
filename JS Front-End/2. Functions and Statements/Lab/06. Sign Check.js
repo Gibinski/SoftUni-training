2ac5af94dd2b0250f5d407c755c9c69679ff2af8
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-15 07:39:00 +0100

    Add

--- JS Front-End/2. Functions and Statements/Lab/06. Sign Check.js
+++ JS Front-End/2. Functions and Statements/Lab/06. Sign Check.js
@@ -1,7 +1,10 @@
-function signCheck() {
-
+function signCheck(...numbers) {
+    return numbers
+        .filter((num) => num < 0)
+        .length % 2 === 0 ? 'Positive': "Negative"
 }
 
-console.log(signCheck());
-console.log(signCheck());
-console.log(signCheck());
+console.log(signCheck( 5,12,-15));
+console.log(signCheck(-6,-12,14));
+console.log(signCheck(-1,-2,-3));
+console.log(signCheck(-5,1, 1));
