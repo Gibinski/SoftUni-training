3ddf48096426a432e3a685c34ada41f14fae3360
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-15 04:52:01 +0100

    Add

---
+++ JS Front-End/2. Functions and Statements/Lab/01. Format Grade.js
@@ -0,0 +1,19 @@
+function formatGrade(grade) {
+    if (grade < 3) {
+        console.log(`Fail (2)`);
+    } else if (grade < 3.5) {
+        console.log(`Poor (${grade.toFixed(2)})`);
+    } else if (grade < 4.5) {
+        console.log(`Good (${grade.toFixed(2)})`);
+    } else if (grade < 5.5) {
+        console.log(`Very good (${grade.toFixed(2)})`);
+    } else {
+        console.log(`Excellent (${grade.toFixed(2)})`);
+    }
+}
+
+formatGrade(2.99);
+formatGrade(3.33);
+formatGrade(4.10);
+formatGrade(4.50);
+formatGrade(5.99);
