9684a0699ae0eb47269adef5bb99d26f43e69cdd
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-15 07:44:15 +0100

    Add

---
+++ JS Front-End/2. Functions and Statements/Exercises/01. Smallest of Three Numbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers() {
+
+}
+
+console.log(smallestOfThreeNumbers(2,5,3));
+console.log(smallestOfThreeNumbers(600,342,123));
+console.log(smallestOfThreeNumbers(25,21,4));
+console.log(smallestOfThreeNumbers(2,2,2));
