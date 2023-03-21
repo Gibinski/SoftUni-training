110a25a9c19b59284fb5e5e29a496430faea78a4
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-21 14:52:49 +0100

    Add

--- JS Front-End/2. Functions and Statements/Exercises/10. Factorial Division.js
+++ JS Front-End/2. Functions and Statements/Exercises/10. Factorial Division.js
@@ -1,7 +1,16 @@
-function factorialDivision() {
-    let ch = 0
+function factorialDivision(firstNumm, secondNum) {
+    function getFactorial(number) {
+        if (number === 1) {
+            return number
+        }
+        return number * getFactorial(number - 1)
+    }
+    let firstFactorial = getFactorial(firstNumm);
+    let secondFactorial = getFactorial(secondNum);
+    let result = firstFactorial / secondFactorial;
+
+    return result.toFixed(2)
  }
 
- console.log(factorialDivision());
- console.log(factorialDivision());
- console.log(factorialDivision());
+ console.log(factorialDivision(5, 2));
+ console.log(factorialDivision(6, 2));
