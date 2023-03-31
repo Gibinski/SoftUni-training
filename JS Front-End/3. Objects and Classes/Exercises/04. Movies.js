baa11b4976c778c96021a69181b4cc15a5dd23a5
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-03-31 23:18:33 +0200

    Add

---
+++ JS Front-End/3. Objects and Classes/Exercises/04. Movies.js
@@ -0,0 +1,33 @@
+function movies() {
+    let movies = [];
+
+    function addMovie(name) {
+        movies.push({ name });
+    }
+
+    function directedBy(name, director) {
+        let movie = movies.find((m) => m.name === name);
+    }
+}
+
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+    ]);
+
+console.log("");
+
+movies([
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'
+    ]);
