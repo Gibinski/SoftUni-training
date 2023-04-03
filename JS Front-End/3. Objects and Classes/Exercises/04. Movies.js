974dc5b71f39ddc4c9fde592eb6a0a8458e2f6f0
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-03 22:31:50 +0200

    Add

--- JS Front-End/3. Objects and Classes/Exercises/04. Movies.js
+++ JS Front-End/3. Objects and Classes/Exercises/04. Movies.js
@@ -1,16 +1,51 @@
-function movies() {
+function moviesParser(input) {
     let movies = [];
+
+    for (let line of input) {
+        let commandTokes = line.split(" ");
+        if (line.includes("addMovie")) {
+            let movieName = commandTokes.slice(1).join(" ");
+            addMovie(movieName);
+        } else if (line.includes("directedBy")) {
+            let indexOfCommand = commandTokes.indexOf("directedBy");
+            let movieName = commandTokes.slice(0, indexOfCommand).join(" ");
+            let director = commandTokes.slice(indexOfCommand + 1).join(" ");
+            addDirector(movieName, director);
+        } else {
+            let indexOfCommand = commandTokes.indexOf("onDate");
+            let movieName = commandTokes.slice(0, indexOfCommand).join(" ");
+            let date = commandTokes.slice(indexOfCommand + 1).join(" ");
+            addDate(movieName, date)
+        }
+    }
+
+    for (const movie of movies) {
+        let check = movie.hasOwnProperty("director") && movie.hasOwnProperty("date");
+        if (check) {
+            console.log(JSON.stringify(movie));
+        }
+    }
 
     function addMovie(name) {
         movies.push({ name });
     }
 
-    function directedBy(name, director) {
+    function addDirector(name, director) {
         let movie = movies.find((m) => m.name === name);
+        if (movie) {
+            movie.director = director;
+        }
+    }
+
+    function addDate(name, date) {
+        let movie = movies.find((m) => m.name === name);
+        if (movie) {
+            movie.date = date
+        }
     }
 }
 
-movies([
+moviesParser([
     'addMovie Fast and Furious',
     'addMovie Godfather',
     'Inception directedBy Christopher Nolan',
@@ -23,7 +58,7 @@ movies([
 
 console.log("");
 
-movies([
+moviesParser([
     'addMovie The Avengers',
     'addMovie Superman',
     'The Avengers directedBy Anthony Russo',
