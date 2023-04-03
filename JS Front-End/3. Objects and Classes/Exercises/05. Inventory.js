ab14770e30fa6fe9899cc53316ead07d656c05b7
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-04 01:17:07 +0200

    Add

---
+++ JS Front-End/3. Objects and Classes/Exercises/05. Inventory.js
@@ -0,0 +1,14 @@
+function inventory(input) {
+
+}
+
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]);
+inventory([
+    'Batman / 2 / Banana, Gun',
+    'Superman / 18 / Sword',
+    'Poppy / 28 / Sentinel, Antara'
+    ]);
