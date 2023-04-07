c5b45ad45abc75d623714f0b3ae03cfae4d255f0
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-07 07:22:11 +0200

    Add

--- JS Front-End/3. Objects and Classes/Exercises/05. Inventory.js
+++ JS Front-End/3. Objects and Classes/Exercises/05. Inventory.js
@@ -1,17 +1,32 @@
 function inventory(input) {
-
-}
-
-let sortedHetoes = herpes
-    .sorted((heroeA, heroeB) => {
-        return heroeA.level - heroeB.level
+    let heroes = [];
+    for (const line of input) {
+        [name, level, itemsStr] = line.split(" / ");
+        items = itemsStr.split(", ");
+        heroes.push({name, level, items})
+    }
+    // sorting heroes by level and name
+    let sortedHeroes = heroes.sort((heroA, heroB) => {
+        let result = heroA.level - heroB.level;
+        if (result === 0) {
+            return heroA.name.localCinoare(heroB.name)
+        }
+        return result
     });
+    // print heroes
+    for (const hero of sortedHeroes) {
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items.join(", ")}`);
+    }
+}
 
 inventory([
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
     'Hes / 1 / Desolator, Sentinel, Antara'
     ]);
+console.log("")
 inventory([
     'Batman / 2 / Banana, Gun',
     'Superman / 18 / Sword',
