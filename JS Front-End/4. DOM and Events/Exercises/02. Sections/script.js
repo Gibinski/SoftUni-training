e12dde0aa7dd6f01d3c4b2fdaeded52a8cc5f17c
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 01:20:03 +0200

    Add

--- JS Front-End/4. DOM and Events/Exercises/02. Sections/script.js
+++ JS Front-End/4. DOM and Events/Exercises/02. Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById("content");
+   for (const text of words) {
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.textContent = text;
+      p.setAttribute("style", "display:none");
+      div.appendChild(p);
+      div.addEventListener("click", clicked);
+      content.appendChild(div);
+   }
+   function clicked(e) {
+      const div = this;
+      div.querySelector("p").setAttribute("style", "display:block");
+   }
 }
