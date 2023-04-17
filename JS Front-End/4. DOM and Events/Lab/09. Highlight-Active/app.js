cc6c0cb4ae3a00e7d5dbce0f69bd6197b9b05a5c
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 00:23:29 +0200

    Add

--- JS Front-End/4. DOM and Events/Lab/09. Highlight-Active/app.js
+++ JS Front-End/4. DOM and Events/Lab/09. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = document.getElementsByTagName("input");
+    for (let i of inputs) {
+        i.addEventListener("focus", focused);
+        i.addEventListener("blur", blurred);
+        
+    }
+
+    function focused(e) {
+        const input = this;
+        input.parentElement.classList.add("focused");
+    }
+
+    function blurred(e) {
+        const input = this;
+        input.parentElement.classList.remove("focused")
+    }
 }
