15eeee0f9f4260bf64f332c8a97125291f686adc
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-17 23:56:00 +0200

    Add

--- JS Front-End/4. DOM and Events/Lab/08. Sum-Table/SumTable.js
+++ JS Front-End/4. DOM and Events/Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    const numbers = document.querySelectorAll("td:nth-child(even)");
+    const sum = document.getElementById("sum");
+    let arr = []
+    for (const td of numbers) {
+        arr.push(td.textContent);
+    }
+    arr.pop();
+    sum.textContent = arr.reduce((a, b) => Number(a) + Number(b), 0)
 }
