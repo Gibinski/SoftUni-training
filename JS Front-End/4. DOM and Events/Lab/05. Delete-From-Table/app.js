b4011ea24f11cd85f424fb9bdd982966832d0540
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-17 19:38:15 +0200

    Add

--- JS Front-End/4. DOM and Events/Lab/05. Delete-From-Table/app.js
+++ JS Front-End/4. DOM and Events/Lab/05. Delete-From-Table/app.js
@@ -1,9 +1,14 @@
 function deleteByEmail() {
-    const email = document.getElementsByName("email").value;
-    let elements = Array.from(document.querySelectorAll("li"));
-    for (const li of elements) {
-        if (li.textContent === email) {
-            let a;
+    const result = document.getElementById("result");
+    const input = document.querySelector("input[name=email");
+    const evenTds = document.querySelectorAll("td:nth-child(even)");
+    let emailValue = input.value;
+    for (let li of evenTds) {
+        if (li.textContent === emailValue) {
+            li.parentElement.remove();
+            result.textContent = "Delete";
+            return;
         }
-    }
+    }        
+    result.textContent = 'Not found.';
 }
