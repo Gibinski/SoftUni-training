84704501e70366bbc25d14c49e5a30139dc4fe12
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-17 14:48:48 +0200

    Add

--- JS Front-End/4. DOM and Events/Lab/01. Sum-Numbers/SumNumbers.js
+++ JS Front-End/4. DOM and Events/Lab/01. Sum-Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
-function calc() {
-    // TODO: sum = num1 + num2
+function calc(n) {
+    const num1 = document.getElementById("num1").value
+    const num2 = document.getElementById("num2").value
+    document.getElementById("sum").value = String(Number(num1) + Number(num2))
 }
+
