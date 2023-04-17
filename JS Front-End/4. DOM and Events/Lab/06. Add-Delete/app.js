1484b4494323c89eff3a8fc96609a3bb80fe73aa
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-17 20:39:41 +0200

    Add

--- JS Front-End/4. DOM and Events/Lab/06. Add-Delete/app.js
+++ JS Front-End/4. DOM and Events/Lab/06. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById("items")
+    const input = document.getElementById("newItemText");
+    const newLi = document.createElement("li");
+    const newAncher = document.createElement("a");
+    newLi.textContent = input.value;
+    newAncher.textContent = "[Delete]";
+    newAncher.setAttribute("href", "#")
+    newAncher.addEventListener("click", deleteHandler);
+    newLi.appendChild(newAncher);
+    ulContainer.appendChild(newLi);
+    input.value = "";
+
+    function deleteHandler(e) {
+        e.currentTarget.parentElement.remove();
+    }
 }
