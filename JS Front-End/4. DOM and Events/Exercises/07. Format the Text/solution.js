7f2984d89d081d552528cae899c816771e1bcc66
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 15:29:04 +0200

    Add

--- JS Front-End/4. DOM and Events/Exercises/07. Format the Text/solution.js
+++ JS Front-End/4. DOM and Events/Exercises/07. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const outputDiv = document.getElementById("output");
+  const textArea = document.getElementById("input");
+  let input = textArea.value;
+  let sentences = input.split(".");
+  sentences.pop();
+
+  while (sentences.length > 0) {
+    const p = document.createElement("p");
+    let sentence = sentences.splice(0, 3);
+    p.textContent = sentence.join(". ") + ".";
+    outputDiv.appendChild(p)
+  }
+
+  textArea.value = "";
 }
