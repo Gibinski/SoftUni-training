f54d02f6b92567148ad8e475adf930b5e712dbe3
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 02:05:07 +0200

    Add

--- JS Front-End/4. DOM and Events/Exercises/03. Accordion/accordion.js
+++ JS Front-End/4. DOM and Events/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.getElementsByClassName("button")[0];
+    const extra = document.getElementById("extra");
+    if (extra.style.display === "none") {
+        button.textContent = "Less";
+        extra.style.display = "block";
+    } else {
+        button.textContent = "More";
+        extra.style.display = "none";
+    }
 }
