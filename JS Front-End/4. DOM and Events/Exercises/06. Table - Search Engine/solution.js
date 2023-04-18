e93d5f379eb51ff867acab62d25c3eb5b83be91f
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 14:06:48 +0200

    Add

--- JS Front-End/4. DOM and Events/Exercises/06. Table - Search Engine/solution.js
+++ JS Front-End/4. DOM and Events/Exercises/06. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
+   const searchInput = document.getElementById("searchField");
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchedWord = searchInput.value;
+      const tableRows = Array.from(document.querySelectorAll("tbody tr"));
+      for (const row of tableRows) {
+         let trimmedText = row.textContent.trim();
 
+         if (row.classList.contains("select")) {
+            row.classList.remove("select");
+         }
+         
+         if (trimmedText.includes(searchedWord)) {
+            row.classList.add("select");
+         }
+      }
+
+      searchInput.value = "";
    }
 }
