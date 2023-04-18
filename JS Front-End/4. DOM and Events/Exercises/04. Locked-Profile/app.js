0312682a40c624908f340289bd9e59e1e6c2f80b
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 10:25:45 +0200

    Add

--- JS Front-End/4. DOM and Events/Exercises/04. Locked-Profile/app.js
+++ JS Front-End/4. DOM and Events/Exercises/04. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.getElementsByTagName("button"));
+
+    for (const button of buttons) {
+        button.addEventListener("click", toggleInformation);
+    }
+
+    function toggleInformation(e) {
+        const btn = this;
+        const profile = Array.from(btn.parentElement.children);
+        const userHiddenFields = profile[9];
+        const unlock = profile[4];
+        
+        if (unlock.checked) {
+            if (btn.textContent === "Show more") {
+                userHiddenFields.style.display = "block";
+                btn.textContent = "Hide it";
+            } else {
+                userHiddenFields.style.display = "none";
+                btn.textContent = "Show more";
+            }
+        }     
+    }
 }
