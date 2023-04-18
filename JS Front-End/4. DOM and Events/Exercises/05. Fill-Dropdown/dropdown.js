9607b8a625e2c10bb5d135285597b13a1178ccec
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-04-18 10:42:58 +0200

    Addd

--- JS Front-End/4. DOM and Events/Exercises/05. Fill-Dropdown/dropdown.js
+++ JS Front-End/4. DOM and Events/Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const newOption = document.createElement("option");
+    const inputText = document.getElementById("newItemText");
+    const inputValue = document.getElementById("newItemValue");
+    const menu = document.getElementById("menu");
+
+    newOption.textContent = inputText.value;
+    newOption.value = inputValue.value;
+    menu.appendChild(newOption);
+
+    inputText.value = "";
+    inputValue.value = "";
 }
