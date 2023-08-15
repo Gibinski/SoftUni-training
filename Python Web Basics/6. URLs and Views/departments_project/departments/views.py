1d1e125ed8d1744d552cfac7df60993c215ab085
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-08-15 14:40:46 +0200

    Add

--- Python Web Basics/6. URLs and Views/departments_project/departments/views.py
+++ Python Web Basics/6. URLs and Views/departments_project/departments/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):  
@@ -7,4 +8,20 @@ def index(request):
 def details(request, department_id):
     print(department_id)
     
-    return HttpResponse('details')
+    department_map = {
+        '1': "Developers",
+        '2': "QA"
+    }
+
+    payload = f"Department: {department_map.get(str(department_id), 'Unknown')}"
+    return HttpResponse(payload)
+
+
+def details_template(request, department_id):
+    department_map = {
+        '1': "Developers",
+        '2': "QA"
+    }
+
+    payload = f"Department: {department_map.get(str(department_id), 'Unknown')}"
+    return render(request, '')
