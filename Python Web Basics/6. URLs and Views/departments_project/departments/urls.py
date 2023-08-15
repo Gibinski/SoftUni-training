5f0caabbc1569d66f099596979b75fe0861c3e3f
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-08-15 21:24:43 +0200

    add

--- Python Web Basics/6. URLs and Views/departments_project/departments/urls.py
+++ Python Web Basics/6. URLs and Views/departments_project/departments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import details, details_template, index
 
 urlpatterns = [
     path('', index),
-    path('<int:department_id>', details),
+    path('int/<int:department_id>', details),
     path('template/<int:department_id>', details_template),
+    path('<department_id>', details),
 ]
