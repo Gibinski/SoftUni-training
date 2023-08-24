4ff9186e49e9f0b28f2b7fa9569653c988211f0f
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-08-24 14:11:34 +0200

    Add url list

--- Python Web Basics/5. Django Introduction/tasks/views.py
+++ Python Web Basics/5. Django Introduction/tasks/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
+from .models import Tasks
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    return HttpResponse("It's works!")
+
+def list_tasks(request):
+    all_tasks = Tasks.objects.all()
+    output = "; ".join(f"{t.id} {t.title} {t.priority}" for t in all_tasks)
+    if not output:
+        output = "There ate no created tasks!"
+    return HttpResponse(output)
+
+
+def list_tasks_template(request):
+    return render(request, 'tasks.html')
