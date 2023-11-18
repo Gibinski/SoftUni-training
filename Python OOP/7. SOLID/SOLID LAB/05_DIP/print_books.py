c7a721c2f41b43173dced4dcb1c437fc884c49b0
Gibinski <l.gibinski@gmail.com>
2023-11-18 14:27:27 -0800

    Add

--- Python OOP/7. SOLID/SOLID LAB/05_DIP/print_books.py
+++ Python OOP/7. SOLID/SOLID LAB/05_DIP/print_books.py
@@ -1,15 +1,30 @@
+from  abc import ABC, abstractmethod
+
+
 class Book:
     def __init__(self, content: str):
         self.content = content
 
 
-class Formatter:
+class Formatter(ABC):
     def format(self, book: Book) -> str:
         return book.content
 
 
+class PressFormatter:
+    def format(self, book: Book) -> str:
+        return book.content[:20]
+        
+
+class EmailFormater:
+    def format(self, book: Book) -> str:
+        return book.content[:20]
+
+
 class Printer:
     def get_book(self, book: Book):
-        formatter = Formatter()
         formatted_book = formatter.format(book)
         return formatted_book
+    
+
+formatter = Formatter()
