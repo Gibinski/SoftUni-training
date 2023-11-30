9741cf169fed20aa235aac873935f061cdec4905
Gibinski <l.gibinski@gmail.com>
2023-11-30 14:11:26 -0800

    Add

--- Python OOP/8. Iterators and Generators/3_vowels.py
+++ Python OOP/8. Iterators and Generators/3_vowels.py
@@ -1,16 +1,16 @@
 class vowels:
-    VOWELS = {"A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"}
+    VOWELS = {"a", "e", "i", "o", "u", "y"}
     def __init__(self, iter_obj) -> None:
-        self.iter_obj = list(filter(lambda i: i in vowels.VOWELS, iter_obj))
-        self.counter = 0
+        self.iter_obj = list(filter(lambda i: i.lower() in vowels.VOWELS, iter_obj))
+        self.current_index = 0
     def __iter__(self):
         return self
     
 
     def __next__(self):
-        if self.counter < len(self.iter_obj):
-            result = self.iter_obj[self.counter]
-            self.counter += 1
+        if self.current_index < len(self.iter_obj):
+            result = self.iter_obj[self.current_index]
+            self.current_index += 1
             return result
         else:
             raise StopIteration
